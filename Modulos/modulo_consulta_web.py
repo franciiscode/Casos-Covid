efa330a425a0e52efdd5257870eebe3ecb53c9e1
Francisco Ramírez <46272555+franciiscode@users.noreply.github.com>
2024-05-14 13:42:53 -0600

    Avance submenu consulta api

--- Modulos/modulo_consulta_web.py
+++ Modulos/modulo_consulta_web.py
@@ -10,23 +10,21 @@ os.system('cls' if os.name == 'nt' else 'clear')
 clear_console()
 """
 
-def consultar_totales_mx():
+def consultar_totales_paises(pais):  # noqa: F821
     """
-    Devuelve la información en una lista de los casos totales de COVID-19 en México. 
+    Devuelve la información de los casos totales de COVID-19 de algun pais en especifico. 
     """
   
     print("Casos totales del COVID-19 en México")
     #Solicitud con GET de los casos totales para Mexico
-    url = "https://disease.sh/v3/covid-19/countries"
+    url = f'https://disease.sh/v3/covid-19/countries/{pais}?strict=false'
     response = requests.get(url)
-
-    #Validad respuesta
     if response.status_code == 200:
         print("La solicitud fue exitosa!")
     else:
         print("Hubo un problema con la solicitud: Código de estado", response.status_code)
     
-    #Se almacenan los datos json en una lista y se devuelven 
+    #Se almacenan los datos json recibidos
     data = response.json() 
     return data
     
@@ -50,18 +48,63 @@ def consultar_historicos_mx():
     data = response.json() 
     return data
 
-def consultar_casos_nl():
+def consultar_globales():
     """
-    Devuelve la información en una lista de los casos históricos de COVID-19 en México del 22/1/20
-    a 9/3/23 para el estado de Nuevo León.
+    Devuelve la información de los casos totales para todos los paises.
     """
-    pass
+    print("Casos globales de COVID-19")
+    #Solicitud con GET de los casos totales globales
+    url = "https://disease.sh/v3/covid-19/countries"
+    response = requests.get(url)
+
+    #***Pendiente validar ***
+    if response.status_code == 200:
+        print("La solicitud fue exitosa!")
+    else:
+        print("Hubo un problema con la solicitud: Código de estado", response.status_code)
+
+    #Recolectar los datos en un json
+    data = response.json()
+    return data
+
+def consultar_globales_historicos():
+    """
+    Devuelve la información de los casos globales acumulados historicos.
+    """
+    print("Casos globales históricos de COVID-19 en todos los paises")
+    #Solicitud con GET de los casos globales historicos
+    url = "https://disease.sh/v3/covid-19/historical/all?lastdays=all"
+    response = requests.get(url)
+
+    #Validar respuesta
+    if response.status_code == 200:
+        print("La solicitud fue exitosa!")
+    else:
+        print("Hubo un problema con la solicitud: Código de estado", response.status_code)
+    
+    #Se almacenan los datos json recibidos
+    data = response.json() 
+    return data
 
 def consultar_vacunas():
     """
-    Devuelve la información en una lista de los casos históricos de COVID-19 en México del 22/1/20
-    a "9/3/23"  
+    Devuelve la información de las dosis totales de vacunas administradas en Mexico de 1/12/20-presente,
+    reportadas por el gobierno.
     """
-    pass
+    print("Dosis totales de vacunas administradas en Mexico")
+    #Solicitud con GET de las vacunas
+    url = "https://disease.sh/v3/covid-19/vaccine/coverage/countries/Mexico?lastdays=all&fullData=false"
+    response = requests.get(url)
+
+    #Validar respuesta
+    if response.status_code == 200:
+        print("La solicitud fue exitosa!")
+    else:
+        print("Hubo un problema con la solicitud: Código de estado", response.status_code)
+    
+    #Se almacenan los datos json recibidos
+    data = response.json() 
+    return data
  
+#
 
--- reporte_casos.py
+++ reporte_casos.py
@@ -1,9 +1,9 @@
-#Modulos usados
+#Modulos
 import sys
 sys.path.append('Modulos') #Agrega los directorios de los modulos al PATH de python
 import pandas as pd
 import modulo_consulta_web as mcw
-import modulo_consulta_registros as mcr
+#import modulo_consulta_registros as mcr
 #import modulo_estadisticas as mstats
 #import modulo_graficas as mplots
 
@@ -11,35 +11,46 @@ import modulo_consulta_registros as mcr
 #MENU PRINCIPAL
 def mostrar_menu_principal():
     """
-    Método para mostrar las opciones del menú principal.
+    Muestra las opciones del menú principal.
     """ 
     print("\n--- Menú Principal ---")
-    print("1. Consultar casos COVID-19")
+    print("1. Consultar casos del COVID-19")
     print("2. Consultar registros")
     print("3. Estadísticas")
     print("4. Gráficas")
-    print("4. Salir")
+    print("5. Eliminar registros")
+    print("6. Salir del programa")
 
 def menu_principal():
-    
+    """
+    Invoca las opciones del menu principal que el usuario elije.
+    """ 
     while True:
         mostrar_menu_principal()
         opcion = input("Seleccione una opción: ")
 
-        if opcion == '1':
+        if opcion == '1': #consultar casos
             submenu_consulta_web()
-        elif opcion == '2':
-            submenu_opcion2()
-        elif opcion == '3':
-            submenu_opcion3()
-        elif opcion == '4':
+        elif opcion == '2': #consultar registros
+            pass
+            #submenu_opcion2()
+        elif opcion == '3': #estadisticas
+            pass
+            #submenu_opcion3()
+        elif opcion == '4': #graficas
+            pass
+        elif opcion == '5':#borrar datos
+            pass
+        elif opcion == '6': #Salir del programa
+            print("Has salido del programa.")
             break
-        else: #***PENDIENTE VALIDACIÓN***
-            print("Opción no válida. Elije una opción del 1 al 4.")
+        else: 
+            print("Opción no válida. Elije una opción del 1 al 6.")
 
 #OPCIONES DEL MENU PRINCIPAL
-def mostrar_submenu_helper(names_list):
-    """ Muestra las subopciones para cada opcion del menú principal"""
+def mostrar_submenu(names_list):
+    """ Muestra las subopciones para cada opcion del menú principal
+    """
     print("\n--- Submenú {} ---\n".format(names_list[0]["submenu_name"]))
     print("Selecciona una opción a consultar")
     
@@ -47,58 +58,97 @@ def mostrar_submenu_helper(names_list):
     for opcion in range(1,len(names_list)):
         print("{}. {}".format(opcion,names_list[opcion]))
     print("R. Regresar al Menú Principal")
-#data_totales = None #Guarda los datos recibidos del
-#data_historicos = None
+
 def submenu_consulta_web():
+    """Invoca las opciones"""
     while True:
         #Se manda la lista de los nombres de las subopciones del menu
         subopciones_names= [{"submenu_name":"Consulta Web"},
                             "Casos totales de COVID-19 en México",
-                            "Casos COVID-19 en Mexico (series temporales)",
-                            "Casos totales en Nuevo León",
-                            "Dosis de vacunas administradas en México"
+                            "Casos históricos totales de COVID-19 en Mexico",
+                            "Casos totales de COVID-19 para todos los paises",
+                            "Casos globales históricos COVID-19",
+                            "Dosis de vacunas administradas en México",
                             ]
-        mostrar_submenu_helper(subopciones_names) 
-        opcion = input("Seleccione una opción: ")
+        mostrar_submenu(subopciones_names) 
+        opcion = input('Seleccione una opción: ')
 
         if opcion == '1':
-            print("Ha seleccionado {}".format(subopciones_names[0]["submenu_name"]))
-            data_totales = mcw.consultar_totales_mx()
+            country = input('Pais a buscar')
             
-            #Guardamos los datos recibidos en un dataframe
-            df_totales = pd.DataFrame(data_totales)
-            print(df_totales.head(20))
+            data_totales = mcw.consultar_totales_paises(country) # "dict" data de la api
+            #Se cambian los nombres y eliminan los datos innecesarios del JSON recibido 
+            data_totales.pop("updated") 
+            data_totales = {
+                "1":["pais",data_totales["country"]],
+                "2":["poblacion",data_totales["population"]],
+                "3":["casos totales",data_totales["cases"]],
+                "4":["casos de hoy",data_totales["todayCases"]],
+                "5":["muertes totales",data_totales["deaths"]],
+                "6":["muertes de hoy",data_totales["todayDeaths"]],
+                "7":["casos recuperados",data_totales["recovered"]],
+                "8":["casos recuperados_hoy", data_totales["todayRecovered"]],
+                "9":["casos activos", data_totales["active"]],
+                "10":["casos criticos", data_totales["critical"]],
+                "11":["pruebas totales", data_totales["tests"]],
+                "12":["pruebas por millon", data_totales["testsPerOneMillion"]],
+                "13":["casos por millon", data_totales["casesPerOneMillion"]],
+                "14":["muertes por millon", data_totales["deathsPerOneMillion"]]
+            }   
+            #Se construye un DataFrame con los datos recibidos para mostrar la info
+            df_totales = pd.DataFrame.from_dict(data_totales, orient = 'Index', columns= ["Nombre", "Valor"])
+            df_totales.index.name = None
+            print('--------------------------------------')
+            print(df_totales)
+            print('--------------------------------------')
+            print('\n(Datos de COVID-19 procedentes de Worldometers, actualizados cada 10 minutos)')  
+                    
         elif opcion == '2':
-            print("Ha seleccionado {}".format(subopciones_names[0]["submenu_name"]))
-            data_historicos = mcw.consultar_historicos_mx()
-            #Guardamos los datos recibidos en un dataframe
-            print(data_historicos)
+            data_historicos_mx = mcw.consultar_historicos_mx()
+            #Se guardan los datos recibidos en un dataframe
+            print(data_historicos_mx)
+            
+            
+            
         elif opcion == '3':
+            data_globales = mcw.consultar_globales() #list
+            
+            df_globales = pd.DataFrame(data_globales) # "DataFrame"
+            #Se eliminan las columnas que no se necesitan del dataframe
+            df_globales.drop(labels =["updated", "countryInfo", "testsPerOneMillion", "oneCasePerPeople", "oneDeathPerPeople",
+                                          "oneTestPerPeople", "activePerOneMillion", "recoveredPerOneMillion", "criticalPerOneMillion"],
+                                 axis = 1, inplace = True )
+
+            #Cambiamos los nombres de las columnas e imprimimos el dataframe
+            df_globales.columns = ["País", "Casos", "CasosDeHoy", "Muertes", "MuertesDeHoy", "Recuperados","RecuperadosDeHoy", "Activos",
+                                       "Críticos", "CasosPorMillon", "MuertesPorMillon","Pruebas", "Población", "Continente"]
+            print(df_globales)
+            
+            
+        elif opcion == '4':
+            
+            data_globales_historicos = mcw.consultar_globales_historicos()
+            print(data_globales_historicos)
+            print(type(data_globales_historicos))
+            
+        elif opcion == '5':
+            data_vacunas = mcw.consultar_vacunas()
+            print(data_vacunas)
+            print(type(data_vacunas))
+            
+        elif opcion == 'R':
             break
+             
         else:
-            print("Opción no válida. Por favor, elija una opción del 1 al 3.")
-#sub-menú para consultar los registros hechos de la función .submenu_consulta_web()
+            print("Opción no válida. Por favor, elija una opción del 1 al 5.")
+            
+#submenú para consultar los registros hechos de la función .submenu_consulta_web()
 def submenu_consulta_registros():
     pass
     """
-    while True:
-        subopciones_names = [] #Agregar nombres de las opciones que tendrá el submenu estadisticas
-        mostrar_submenu_helper(subopciones_names) 
-        opcion = input("Seleccione una opción: ")
-        
-        if opcion == '1':
-            #print("Ha seleccionado {}".format(subopciones_names[1]))
-            pass
-        elif opcion == '2':
-            #print("Ha seleccionado {}".format(subopciones_names[2]))
-            pass
-        elif opcion == '3':
-            #print("Ha seleccionado {}".format(subopciones_names[3]))
-            break
-        else:
-            print("Opción no válida. Por favor, elija una opción del 1 al 3.")
+    Pendiente
     """
-#sub-menú para consultar las estadísticas
+#submenú para consultar las estadísticas
 def submenu_estadisticas():
     pass
     """
@@ -119,7 +169,7 @@ def submenu_estadisticas():
         else:
             print("Opción no válida. Por favor, elija una opción del 1 al 3.")
     """
-#sub-menú para consultar las graficas
+#submenú para consultar las graficas
 def submenu_graficas():
     pass
     """
@@ -141,8 +191,9 @@ def submenu_graficas():
             print("Opción no válida. Por favor, elija una opción del 1 al 3.")
     """
 ##Metodo para borrar todos los registros
-def borrar_registros():
+def submenu_borrar_registros():
     pass
+
 if __name__ == "__main__":
     menu_principal() #iniciar el programa
 
